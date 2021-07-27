8f0a7684b4e79f968dcb9186dfab717cf2e4fd7f
Sean P. Goggins <s@goggins.com>
2021-07-27 13:02:14 -0500

    syntax fix.

--- workers/github_worker/github_worker.py
+++ workers/github_worker/github_worker.py
@@ -92,9 +92,7 @@ class GitHubWorker(WorkerGitInterfaceable):
                             'augur': ['gh_node_id']
                         }
                     }, prefix='user.'
-                    self.logger.info(
-                    "Action map additions "
-                    f"{(action_map_additions)}.\n")
+                    self.logger.info(f"Action map additions {action_map_additions}.")
                 )
             except ValueError:
                 self.logger.info(f"Enrich contrib data is empty for {inc_source_issues['insert']}, the empty field is the user login.")
